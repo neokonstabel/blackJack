75a82554fd3b4d2ee9aba11b4c5b441a8529a551
neokonstabel <konstabelneo@gmail.com>
2022-12-13 21:28:29 +0200

    The getRandCard() functionality made better

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
-let firstCard = 11;
-let secondCard = 8;
+let firstCard = getRandomCard() ;
+let secondCard = getRandomCard();
 let sum = firstCard + secondCard;
+let cardsArray = [firstCard,secondCard]
 let hasBlackJack = false;
 let isAlive = true;
 let message = "";
 let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
+let cardsEl = document.getElementById("cards-el");
 
-function startGame() {
+function getRandomCard(){
+  let randomNumber = Math.floor(Math.random() * 14+ 1)
+  return randomNumber
+}
+
+function startGame(){
+  renderGame()
+}
+
+function renderGame() {
+  sumEl.textContent = "Sum: " + sum;
+  cardsEl.textContent = "Cards: "
+  for (let i = 0; i < cardsArray.length; i++) {
+    cardsEl.textContent += cardsArray[i] + " "
+    
+  }
   if (sum <= 20) {
     message = "Do you want another draw?";
   } else if (sum === 21) {
@@ -18,6 +35,13 @@ function startGame() {
     isAlive = false;
   }
   messageEl.textContent = message;
-  sumEl.textContent = "Sum: " + sum;
   console.log(message);
 }
+
+function newCard() {
+  let newCard = getRandomCard();
+  sum += newCard
+  cardsArray.push(newCard)
+  renderGame()
+}
+
