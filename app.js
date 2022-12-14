8cfda989166b78b5a4ee7799452c2be780a2e109
neokonstabel <konstabelneo@gmail.com>
2022-12-14 20:33:35 +0200

    improvement on newCard()

--- app.js
+++ app.js
@@ -1,29 +1,44 @@
-let firstCard = getRandomCard() ;
-let secondCard = getRandomCard();
-let sum = firstCard + secondCard;
-let cardsArray = [firstCard,secondCard]
+let player = {
+  name: "Nota",
+  chips: 45,
+};
+let sum = 0;
+let cardsArray = [];
 let hasBlackJack = false;
-let isAlive = true;
+let isAlive = false;
 let message = "";
 let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
 let cardsEl = document.getElementById("cards-el");
+let playerEl = document.getElementById("player-el");
 
-function getRandomCard(){
-  let randomNumber = Math.floor(Math.random() * 14+ 1)
-  return randomNumber
+playerEl.textContent = player.name + ": $ " + player.chips;
+
+function getRandomCard() {
+  let randomNumber = Math.floor(Math.random() * 13 + 1);
+  if (randomNumber > 10) {
+    return 10;
+  } else if (randomNumber === 1) {
+    return 11;
+  } else {
+    return randomNumber;
+  }
 }
 
-function startGame(){
-  renderGame()
+function startGame() {
+  isAlive = true;
+  let firstCard = getRandomCard();
+  let secondCard = getRandomCard();
+  cardsArray = [firstCard, secondCard];
+  sum = firstCard + secondCard;
+  renderGame();
 }
 
 function renderGame() {
   sumEl.textContent = "Sum: " + sum;
-  cardsEl.textContent = "Cards: "
+  cardsEl.textContent = "Cards: ";
   for (let i = 0; i < cardsArray.length; i++) {
-    cardsEl.textContent += cardsArray[i] + " "
-    
+    cardsEl.textContent += cardsArray[i] + " ";
   }
   if (sum <= 20) {
     message = "Do you want another draw?";
@@ -39,9 +54,10 @@ function renderGame() {
 }
 
 function newCard() {
-  let newCard = getRandomCard();
-  sum += newCard
-  cardsArray.push(newCard)
-  renderGame()
+  if (isAlive && !hasBlackJack) {
+    let newCard = getRandomCard();
+    sum += newCard;
+    cardsArray.push(newCard);
+    renderGame();
+  }
 }
-
